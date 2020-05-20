db7e741350bd0fb653e611e1179c08b273720d4d
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-05-20 16:02:09 +0900

    [ログイン確認]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+
+	def new
+		@book = Book.new
+		@user = User.find(params[:id])
+	end
+
+	def create
+		@book = Book.new(user_params)
+		@book.user_id = current_user.id
+		@book.save
+		redirect_to book_path(@book.id)
+	end
+
+	def index
+		@books = Book.all
+		@book = Book.new
+	end
+
+	def show
+		@book =Book.find(params[:id])
+		
+	end
+
+	def edit
+	end
+
+	def destroy
+		@book = Book.find(params[:id])
+		@book.destroy
+		redirect_to books_index
+	end
+
+	private
+	def user_params
+		params.require(:book).permit(:title, :body)
+	end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+         has_many :books, dependent: :destroy
+
+         validates :name, length: {in:2..20}
+         validates :introduction, length: {maximum: 50}
+
+         #attachment :profile_image
+
+         def posts
+         	return Book.where(user_id: self.id)
+         end
+end
